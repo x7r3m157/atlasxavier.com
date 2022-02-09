8e2c06ab1f1ea7efb549b83fcb9b0a4e8ba634f7
Atlas Atendido <x7r3m157@Atlass-MacBook-Pro.local>
2022-02-09 13:10:22 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+      title: `atlasxavier.com`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-3EJV664NT4"
+    }
+  }]
+};
