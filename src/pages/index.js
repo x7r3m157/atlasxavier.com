68fbe71ed4befecc9dcda4e7313d64c32a0adaf4
Atlas Atendido <x7r3m157@Atlass-MacBook-Pro.local>
2022-02-17 13:35:36 -0800

    testing oura API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, {useEffect, useState} from "react";
 
 import atlas from '../images/atlas.png'
 
@@ -6,32 +6,65 @@ import {
   container,
   atlasStyles,
   nav,
-  navItem
+  navItem,
+  background,
+  content
 } from './index.module.css'
 
 const IndexPage = () => {
 
+  const [oura, setOura] = useState('')
+
+  useEffect(() => {
+
+      var myHeaders = new Headers(); 
+      myHeaders.append('Authorization', 'Bearer Q7XKSFKQBNNKTGI3H7MCEIA3EJUMBLFX'); 
+      var requestOptions = { 
+        method: 'GET', 
+        headers: myHeaders, 
+      }
+
+      const interval = setInterval(() => {
+
+      fetch('https://api.ouraring.com/v2/usercollection/daily_activity?start_date=2021-11-01&end_date=2021-12-01', requestOptions) 
+        .then(response => response.text()) 
+        .then(result => setOura(oura => oura += result)) 
+        .catch(error => console.log('error', error));
+      }, 5000);
+      return () => clearInterval(interval);
+    
+    }, [oura]);
+    
+    
 
   return (
     <>
-      <div className={container}>
-
+        <div className={content}>
           <img
-            className={atlasStyles}
-            alt="Atlas Himself"
-            src={atlas}
-          />
-          <h2>ATLAS XAVIER LEE ATENDIDO</h2>
-          <h3>DESIGN TECHNOLOGIST</h3>
+                className={atlasStyles}
+                alt="Atlas Himself"
+                src={atlas}
+              />
+              <h2>ATLAS XAVIER LEE ATENDIDO</h2>
+              <h3>DESIGN TECHNOLOGIST</h3>
+              
           <div className={nav}>
             <h4 className={navItem}>PROJECTS</h4>
             <h4 className={navItem}>SRS NOTES</h4>
             <h4 className={navItem}>SKILLTREE</h4>
           </div>
-
+          <h3>OURA DATA: {oura} </h3>
+          
       </div>
+      <div className={container}>
+        <div className={background}>
+        </div>
+      </div>
+  
     </>
   )
 }
 
 export default IndexPage
+
+// 89a8d8481639b3de8e37aca4622416b8
